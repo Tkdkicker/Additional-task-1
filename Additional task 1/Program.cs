9f038cb647d1d4655b162f56fe61fe2882a4648c
gemma <gemma@DESKTOP-2Q2G917>
2022-11-14 10:28:24 +0000

    Add project files.

---
+++ Additional task 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Additional_task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How old are you?");
+            int age = int.Parse(Console.ReadLine());
+
+            if (age < 18)
+            {
+                Console.WriteLine("You cannot vote");
+            }
+
+            else
+            {
+                Console.WriteLine("You can vote");
+            }
+        }
+    }
+}
